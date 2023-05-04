f29536be3c95676b1be38114e868597044124cfd
ikafly144 <support@sabafly.net>
2023-05-04 17:53:05 +0900

    update: 絵文字判定を改善

---
+++ emoji/emoji_test.go
@@ -0,0 +1,103 @@
+package emoji_test
+
+import (
+	"testing"
+
+	"github.com/sabafly/sabafly-lib/emoji"
+)
+
+func TestEmoji(t *testing.T) {
+	type args struct {
+		a string
+	}
+	{
+		tests := []struct {
+			name string
+			args args
+			want bool
+		}{
+			{
+				name: "case1",
+				args: args{a: "😁"},
+				want: true,
+			},
+			{
+				name: "case2",
+				args: args{a: "🇯🇵"},
+				want: true,
+			},
+			{
+				name: "case3",
+				args: args{a: "🧑🏽‍🚀"},
+				want: true,
+			},
+			{
+				name: "case4",
+				args: args{a: "1"},
+				want: false,
+			},
+			{
+				name: "case5",
+				args: args{a: "A"},
+				want: false,
+			},
+			{
+				name: "case6",
+				args: args{a: "<:modify:1082025248330891388>"},
+				want: true,
+			},
+		}
+		for _, v := range tests {
+			t.Run(v.name, func(t *testing.T) {
+				if got := emoji.MatchString(v.args.a); got != v.want {
+					t.Errorf("UnicodeEmoji.MatchString() = %v, want %v", got, v.want)
+				}
+			})
+		}
+	}
+	{
+		tests := []struct {
+			name string
+			args args
+			want int
+		}{
+			{
+				name: "case1",
+				args: args{a: "😁"},
+				want: 1,
+			},
+			{
+				name: "case2",
+				args: args{a: `🇯🇵`},
+				want: 1,
+			},
+			{
+				name: "case3",
+				args: args{a: "🧑🏽‍🚀"},
+				want: 1,
+			},
+			{
+				name: "case4",
+				args: args{a: "1"},
+				want: 0,
+			},
+			{
+				name: "case5",
+				args: args{a: "A"},
+				want: 0,
+			},
+			{
+				name: "case6",
+				args: args{a: "<:modify:1082025248330891388>"},
+				want: 1,
+			},
+		}
+		for _, v := range tests {
+			t.Run(v.name, func(t *testing.T) {
+				if got := emoji.FindAllString(v.args.a); len(got) != v.want {
+					t.Errorf("FindAllString() = %#v, len() = %v, want %v", got, len(got), v.want)
+				}
+			})
+		}
+	}
+}
--- go.mod
+++ go.mod
@@ -19,7 +19,9 @@ require (
 require (
 	github.com/cespare/xxhash/v2 v2.1.2 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/forPelevin/gomoji v1.1.8 // indirect
 	github.com/gorilla/websocket v1.5.0 // indirect
+	github.com/rivo/uniseg v0.4.4 // indirect
 	github.com/sasha-s/go-csync v0.0.0-20210812194225-61421b77c44b // indirect
 	golang.org/x/crypto v0.0.0-20220525230936-793ad666bf5e // indirect
 	golang.org/x/exp v0.0.0-20220325121720-054d8573a5d8 // indirect
